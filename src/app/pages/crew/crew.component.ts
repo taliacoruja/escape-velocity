e789a0298d30b4ec0164bd48a542b973f1645c62
n.protasova <rot.fuchslein@gmail.com>
2026-03-09 11:19:44 +0100

    setup project architecture and routing structure

---
+++ src/app/pages/crew/crew.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crew',
+  imports: [],
+  templateUrl: './crew.component.html',
+  styleUrl: './crew.component.scss'
+})
+export class CrewComponent {
+
+}
---
+++ src/app/pages/technology/technology.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-technology',
+  imports: [],
+  templateUrl: './technology.component.html',
+  styleUrl: './technology.component.scss'
+})
+export class TechnologyComponent {
+
+}
